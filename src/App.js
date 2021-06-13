0d1962a3c34d3d907e5f9d2d6e48c2a9ab82df29
unknown <navinkodam11@gmail.com>
2021-06-13 13:03:43 +0530

    the portfolio is made

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Testimonials from "./components/Testimonials";
+import Portfolio from "./components/Portfolio";
+
+import "./App.css";
+
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
+
+export default App;
